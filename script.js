16b1117e7b4024feee69ad820f0740742ddd0293
lyqht <estee.tsw@gmail.com>
2022-10-18 09:29:03 +0800

    add script and utils for removing labels

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 // @ts-check
 
+import { getLabels } from "./utils";
+
 /**
  * Remove labels from all issues of given repositories
  *
  * @param {import('@octoherd/cli').Octokit} octokit
  * @param {import('@octoherd/cli').Repository} repository
- * @param {object} options
- * @param {string} options.repositories One or multiple space-separated repositories in the form of repo-owner/repo-name. repo-owner/* will find all repositories for one owner. * will find all repositories the user has access to. Will prompt for repositories if not set
+ * @param { {labels: string[]} } options Custom user options passed to the CLI
  */
-export async function script(octokit, repository, { repositories }) {}
+export async function script(octokit, repository, { labels }) {
+	const { owner, name } = repository;
+	const repoLabels = await getLabels(octokit, repository);
+	const labelNames = repoLabels.map((l) => l.name);
+	const newLabels = labelNames.filter(
+		(name) => !labels.find((label) => label === name),
+    );
+    let resultLabels = [];
+    if (repoLabels.length != newLabels.length) {
+        resultLabels = await octokit.rest.issues.deleteLabel({
+            repo: repository.name,
+            owner,
+            name,
+        });
+        octokit.log.info(`Labels ${labels} removed from ${repository.name}`);
+    }
+    return resultLabels;
+}
